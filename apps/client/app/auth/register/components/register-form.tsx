f3d55a5bc4845159b6ccce09d3724fc779d86200
Stefan Mijic <arabojacob170@gmail.com>
2025-08-17 15:29:03 +0200

    Fix TypeScript error in register form - correct fillRule attribute

--- apps/client/app/auth/register/components/register-form.tsx
+++ apps/client/app/auth/register/components/register-form.tsx
@@ -221,7 +221,7 @@ export function RegisterForm({
                     ) : (
                       <div className="flex items-center space-x-2 text-orange-600">
                         <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
-                          <path fillRule="round" strokeLinejoin="round" strokeWidth={2} d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                          <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
                         </svg>
                         <span className="text-sm">Passwords do not match</span>
                       </div>
